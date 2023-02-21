57b55fec2cb49573314c5749e0bdc78be5386550
Emmanuel Donkor <sunnycodeengineer@gmail.com>
2023-02-21 20:03:31 +0000

    Initial Commit

---
+++ My AIM App Swiftui/My_AIM_App_SwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_AIM_App_SwiftuiApp.swift
+//  My AIM App Swiftui
+//
+//  Created by Emmanuel Donkor on 21/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct My_AIM_App_SwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
