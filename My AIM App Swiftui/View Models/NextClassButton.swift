dbadd8495040435033e944a8a90df410609522b6
Emmanuel Donkor <sunnycodeengineer@gmail.com>
2023-03-07 19:49:41 +0000

    Working on Next Class Button.

--- My AIM App Swiftui/View Models/NextClassButton.swift
+++ My AIM App Swiftui/View Models/NextClassButton.swift
@@ -18,42 +18,53 @@ struct NextClassButton: View {
         } label: {
             ZStack {
                 ZStack {
-                    HStack {
-                        Spacer()
-                        Image(systemName: "map.fill")
-                            .resizable()
-                            .scaledToFit()
-                            .opacity(0.4)
-                            .rotationEffect(Angle(degrees: -25))
-                            .offset(x: 50, y: 100)
-                    }
-                        
                     
-                    VStack {
-                        Spacer()
-                        HStack(alignment: .bottom) {
-                            VStack(alignment: .leading) {
-                                Text("Next\nClass")
-                                    .font(.title)
-                                HStack {
-                                    Text("Course: ")
-                                    Text(course)
-                                }
-                                HStack {
-                                    Text("Location: ")
-                                    Text(location)
-                                }
-                                HStack {
-                                    Text("Time: ")
-                                    Text(time)
-                                }
-                            }
-                            
+                    ZStack {
+                        VStack {
                             Spacer()
+                            HStack(alignment: .bottom) {
+                                VStack(alignment: .leading) {
+                                    Text("Next\nClass")
+                                        .font(.title)
+                                    HStack {
+                                        Text("Course: ")
+                                        Text(course)
+                                    }
+                                    HStack {
+                                        Text("Location: ")
+                                        Text(location)
+                                    }
+                                    HStack {
+                                        Text("Time: ")
+                                        Text(time)
+                                    }
+                                }
+                                
+                                Spacer()
+                            }
+                            .padding()
+                            .background(RoundedRectangle(cornerRadius: 15)
+                                .fill(.thinMaterial)
+                                .shadow(radius: 5)
+                                .overlay {
+                                    RoundedRectangle(cornerRadius: 15)
+                                        .stroke(.white, lineWidth: 2)
+                                })
                         }
-                        .padding()
-                        .background(.blue.opacity(0.3))
+                        
+                        HStack {
+                            Spacer()
+                            Image(systemName: "map.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .opacity(0.2)
+                                .rotationEffect(Angle(degrees: -25))
+                                .offset(x: 50, y: 100)
+                                .clipped()
+                                .cornerRadius(15)
+                        }
                     }
+                    .padding()
                 }
                 .clipped()
                 
@@ -62,14 +73,14 @@ struct NextClassButton: View {
                     
                     Image(systemName: "mappin")
                         .resizable()
-                    .scaledToFit()
+                        .scaledToFit()
+                        .shadow(radius: 2, x: 3, y: 5)
+                        .offset(y: -20)
                 }
                 .padding()
             }
             .frame(width: UIScreen.main.bounds.width * 0.9,
                    height: 200)
-            .background(.blue.opacity(0.3))
-            
         }
     }
 }
